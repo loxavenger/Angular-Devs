9b8c8b53b2b1ad406f024b7f8784924267cb14d0
Aman <aman@podiotracker.com>
2023-12-30 13:01:00 +0530

    Created all component and routing

--- Wild_Release/src/app/app-routing.module.ts
+++ Wild_Release/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './HomePage/home/home.component';
+import { PageNotFoundComponent } from './HomePage/page-not-found/page-not-found.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { ProjectComponent } from './project/project.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:HomeComponent,pathMatch: 'full'},
+  {path:'home',component:HomeComponent},
+  {path:'About',component:AboutComponent},
+  {path:'Contact',component:ContactComponent},
+  {path:'Project',component:ProjectComponent},
+  {path:'**',component:PageNotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Wild_Release/src/app/app.module.ts
+++ Wild_Release/src/app/app.module.ts
@@ -4,11 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './HomePage/home/home.component';
+import { HeaderComponent } from './HomePage/header/header.component';
+import { FooterComponent } from './HomePage/footer/footer.component';
+import { PageNotFoundComponent } from './HomePage/page-not-found/page-not-found.component';
+import { ContactComponent } from './contact/contact.component';
+import { ProjectComponent } from './project/project.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AboutComponent
+    AboutComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    PageNotFoundComponent,
+    ContactComponent,
+    ProjectComponent
   ],
   imports: [
     BrowserModule,
